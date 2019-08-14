47d39c6fd919bd7cf255dc5d0a6eca276673023f
orthagonal <orthagonal1@gmail.com>
2019-08-14 20:54:40 +0000

    expand paths feature

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const wreck = require('wreck');
 const parser = require('fast-xml-parser');
-
-const options = {};
+const url = require('url');
+const parserOptions = {};
 
 let allParsedSitemaps = [];
 let sitemap2csv;
@@ -14,7 +14,7 @@ const getSitemap = async (url) => {
 const parseXml = async (xmlContent) => {
   const result = parser.validate(xmlContent);
   if (result === true) {
-    const parsedSitemap = parser.parse(xmlContent, options);
+    const parsedSitemap = parser.parse(xmlContent, parserOptions);
     // if it's a sitemap index just get all the listed sitemaps that are indexed:
     if (parsedSitemap.sitemapindex && parsedSitemap.sitemapindex.sitemap) {
       // if there is just one loc it will be a single entry:
@@ -35,7 +35,7 @@ const parseXml = async (xmlContent) => {
 };
 
 // prints parsed sitemap to stdout in csv form;
-const exportCsv = async () => {
+const exportCsv = async (expandPaths) => {
   // do a sweep to get all the field names for the header row:
   const headerRow = {};
   allParsedSitemaps.forEach(entry => {
@@ -57,12 +57,60 @@ const exportCsv = async () => {
   });
 };
 
+// expands the components of each url path into columns:
+const exportCsvExpandPath = async (expandPaths) => {
+  // do a sweep to get all the field names for the header row:
+  const headerRow = {};
+  allParsedSitemaps.forEach(entry => {
+    Object.keys(entry).forEach(key => {
+      headerRow[key] = true;
+    });
+  });
+  const fields = Object.keys(headerRow);
+  // do an additional sweep to get all the path component names needed for the header row:
+  // eg 'folder1,folder2,folder3'
+  //add a 'path' field at the end:
+  headerRow.path = true;
+  allParsedSitemaps.forEach(entry => {
+    const path = new URL(entry.loc).pathname;
+    path.split('/').forEach((component, i) => {
+      // first one will be blank:
+      if (i > 0) {
+        headerRow[`folder${i}`] = true;
+      }
+    });
+  });
+  console.log(Object.keys(headerRow).join(','));
+  // generate each row as csv, include the path and each of its components as well:
+  allParsedSitemaps.forEach(entry => {
+    const row = fields.map(fieldName => {
+      if (entry[fieldName]) {
+        return entry[fieldName];
+      }
+      return '';
+    });
+    const path = new URL(entry.loc).pathname;
+    row.push(path);
+    // store the components of the path as well:
+    path.split('/').forEach((component, i) => {
+      // first one will be blank again:
+      if (i > 0) {
+        row.push(component);
+      }
+    });
+    console.log(row.join(','));
+  });
+};
+
 sitemap2csv = async(url) => {
   const sitemap = await getSitemap(url);
   await parseXml(sitemap);
 };
 
-module.exports = async(url) => {
+module.exports = async(url, expandPaths = false) => {
   await sitemap2csv(url);
-  exportCsv();
+  if (expandPaths) {
+    return exportCsvExpandPath();
+  }
+  return exportCsv();
 };
